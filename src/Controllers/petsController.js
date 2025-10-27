a46a08457a5890a93c93243f924b3e5de907bd3b
Beatriz Tsumoto <beatriz.tsumoto@aluno.senai.br>
2025-10-27 16:23:06 -0300

    feat:atualiza controllers, models e routes

--- src/Controllers/petsController.js
+++ src/Controllers/petsController.js
@@ -54,3 +54,93 @@ export const listarUm = async (req, res) => {
         })
     }
 }
+
+export const criar = async (req, res) => {
+    try {
+        // De onde vem os dados para cá? Para eu usar para criar
+        const { nome, especie, idade, dono} = req.body
+
+        const dado = req.body
+
+        // Validacao campos obrigatorios
+        const camposObrigatorios = ['nome', 'especie', 'idade', 'dono'];
+
+        const faltando = camposObrigatorios.filter(campo => !dado[campo]);
+
+        if (faltando.length > 0) {
+            return res.status(400).json({
+                erro: `Os seguintes campos são obrigatórios: ${faltando.join(', ')}.`
+            });
+        }
+
+        const novoPet = await PetsModel.createBruxo(dado);
+
+        res.status(201).json({
+            mensagem: 'Pet criado com sucesso!',
+            pet: novoPet
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            erro: 'Erro ao criar pet',
+            detalhes: error.message
+        })
+    }
+}
+
+export const apagar = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        const petExiste = await PetsModel.findById(id);
+
+        if (!petExiste) {
+            return res.status(404).json({
+                erro: 'Pet não encontrado com esse id',
+                id: id
+            })
+        }
+
+        await PetsModel.deletePet(id)
+
+        res.status(200).json({
+            mensagem: 'Pet removido com sucesso',
+            petRemovido: petExiste
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            erro: 'Erro ao apagar bruxo!',
+            detalhes: error.message
+        })
+    }
+}
+
+export const atualizar = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const dados = req.body;
+
+        const petExiste = await PetsModel.findById(id);
+
+        if (!petExiste) {
+            return res.status(404).json({
+                erro: 'Pet não encontrado com esse id',
+                id: id
+            })
+        }
+        const petAtualizado = await PetsModel.update(id, dados);
+
+        res.status(200).json({
+            mensagem: 'Pet atualizado com sucesso',
+            pet: petAtualizado
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            erro: 'Erro ao atualizar bruxos',
+            detalhes: error.message
+        })
+    }
+}
--- src/Routes/petsRoute.js
+++ src/Routes/petsRoute.js
@@ -7,4 +7,10 @@ router.get("/", PetsController.listarTodos);
 
 router.get("/:id", PetsController.listarUm);
 
+router.post("/", PetsController.criar);
+
+router.delete("/:id", PetsController.apagar);
+
+router.put("/:id", PetsController.atualizar)
+
 export default router;
